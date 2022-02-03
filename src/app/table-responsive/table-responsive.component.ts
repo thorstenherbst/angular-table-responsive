0210c721715298b790afb00266d6cc39f59c672e
José Luis Olivares <jolivares@gbm.net>
2022-02-03 16:28:16 -0600

    Resolve mat modules dependencies

--- src/app/table-responsive/table-responsive.component.ts
+++ src/app/table-responsive/table-responsive.component.ts
@@ -5,6 +5,8 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
 
+
+
 @Component({
   selector: 'app-table-responsive',
   templateUrl: './table-responsive.component.html',

593811cfdea0af78cbba35950a76a590879009fc
José Luis Olivares <jolivares@gbm.net>
2022-02-03 17:10:18 -0600

    remove space

--- src/app/table-responsive/table-responsive.component.ts
+++ src/app/table-responsive/table-responsive.component.ts
@@ -4,9 +4,6 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
-
-
-
 @Component({
   selector: 'app-table-responsive',
   templateUrl: './table-responsive.component.html',
